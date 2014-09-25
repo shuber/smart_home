6dfe0985eef1ae42f9f2f41e0ceed81a7603745d
Sean Huber <sean@onmyblock.com>
2014-09-24 21:13:33 -0700

    Don't verify the authenticity token for `admin/readings#create`

--- app/admin/reading.rb
+++ app/admin/reading.rb
@@ -1,3 +1,7 @@
 ActiveAdmin.register Reading do
-  permit_params :sensor_id, :value
+  permit_params :sensor_id, :value, :created_at
+
+  controller do
+    skip_before_filter :verify_authenticity_token, only: :create
+  end
 end
