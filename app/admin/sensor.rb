2d04136928616c9fd84dfbef6b829f45b330caed
Sean Huber <sean@onmyblock.com>
2014-09-24 20:20:34 -0700

    Make the `type` field a drop down input of valid types

--- app/admin/sensor.rb
+++ app/admin/sensor.rb
@@ -1,3 +1,14 @@
 ActiveAdmin.register Sensor do
   permit_params :name, :type
+
+  form do |f|
+    f.semantic_errors(*f.object.errors.keys)
+
+    f.inputs do
+      f.input :type, as: :select, collection: Sensor::TYPES
+      f.input :name
+    end
+
+    f.actions
+  end
 end
