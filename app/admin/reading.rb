c7781b80257a6af0beb7da88315f493d9d626d29
Sean Huber <sean@onmyblock.com>
2014-09-24 21:28:37 -0700

    Add  to the admin  index

--- app/admin/reading.rb
+++ app/admin/reading.rb
@@ -1,6 +1,16 @@
 ActiveAdmin.register Reading do
   permit_params :sensor_id, :value, :created_at
 
+  index do
+    column :id
+    column :sensor
+    column :value
+    column :created_at
+    column :updated_at
+
+    actions
+  end
+
   controller do
     skip_before_filter :verify_authenticity_token, only: :create
   end
