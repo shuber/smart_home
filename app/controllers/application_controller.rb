2b47e62ca4e83b3892d4ec6d876caa29464c5d82
Sean Huber <sean@onmyblock.com>
2014-09-24 20:24:43 -0700

    Add basic authentication for a single `admin` user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def authenticate_admin_user!
+    authenticate_or_request_with_http_basic do |username, password|
+      valid_username = 'admin'
+      valid_password = '2ed92f0f5cfd4f5bc50cc2810f63be16ef5b9732e660d5db40deca887547344a'
+      hashed_password = Digest::SHA2.hexdigest password
+
+      valid_username == username && valid_password == hashed_password
+    end
+  end
 end
