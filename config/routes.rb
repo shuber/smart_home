108248d0935d6475d5fd41d1742dee3043a275d5
Sean Huber <sean@onmyblock.com>
2014-09-24 21:04:00 -0700

    Redirect the root path to `/admin`

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   ActiveAdmin.routes(self)
+  root to: redirect("/admin")
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
