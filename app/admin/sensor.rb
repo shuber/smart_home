6264802a9c34787e988d503758204084fc706d8f
Sean Huber <sean@onmyblock.com>
2014-09-24 23:18:25 -0700

    Reorganize the admin index columns for sensors

--- app/admin/sensor.rb
+++ app/admin/sensor.rb
@@ -1,6 +1,20 @@
 ActiveAdmin.register Sensor do
   permit_params :name, :type
 
+  index do
+    column :type
+
+    column "Last updated" do |sensor|
+      since = time_ago_in_words sensor.created_at
+      "#{since} ago"
+    end
+
+    column :name
+    column :created_at
+    column :id
+    actions
+  end
+
   form do |f|
     f.semantic_errors(*f.object.errors.keys)
 
